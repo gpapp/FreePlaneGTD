653780ae39ef27ee0488ec1d8ee5401c77d5448c
Gergely Papp <gergely.papp@itworks.hu>
2017-10-27 17:23:45 +0200

    Verbose URL handler, to be removed later

--- lib/src/freeplaneGTD/protocol/fpgtd/Handler.groovy
+++ lib/src/freeplaneGTD/protocol/fpgtd/Handler.groovy
@@ -5,11 +5,18 @@ import java.net.URL
 import java.net.URLConnection
 import java.net.URLStreamHandler
 import org.freeplane.core.resources.ResourceController
+import java.util.logging.Logger
+import java.util.logging.Level
 
 class Handler extends URLStreamHandler {
+static {
+            Logger.getAnonymousLogger().log(Level.INFO, "Protocol handler initialized")
+}
 	@Override
 	protected URLConnection openConnection(URL u) throws IOException {
 	    ResourceController resourceController = ResourceController.getResourceController()
-		return resourceController.getResource("/images/icons/" + u.getPath()).openConnection()
+		logger.info("Loading resource:" + u.path)
+
+		return resourceController.getResource("/images/icons/" + u.path).openConnection()
 	}
 }
