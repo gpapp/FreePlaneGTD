abd51b6ac6563f568c8c17923226b39b6a2daed2
Papp Gergely <gergely.papp@itworks.hu>
2018-01-30 18:58:58 +0100

    Restored files removed on branch merge by mistake

---
+++ src/resources/scripts/ReviewTask.groovy
@@ -0,0 +1,25 @@
+// @ExecutionModes({on_single_node="main_menu_scripting/freeplaneGTD[addons.archiveTask]"})
+/*
+=========================================================
+ Freeplane GTD+
+
+ Copyright (c)2016 Gergely Papp
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+=========================================================
+*/
+freeplaneGTD.mover.ReviewTask reviewTask = new freeplaneGTD.mover.ReviewTask()
+reviewTask.execute(freeplaneGTD.mover.ReviewTask.findOrCreateReviewDir(node), node)
+
