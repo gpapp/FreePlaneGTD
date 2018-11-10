71a2eac000f9649d86fd250a6dc832807ffe63e3
Gergely Papp <gergely.papp@itworks.hu>
2018-11-10 08:19:30 +0100

    Changing Listeners to 1.7

--- src/resources/zips/scripts/init/AddGTDChangeListener.groovy
+++ src/resources/zips/scripts/init/AddGTDChangeListener.groovy
@@ -1,11 +1,20 @@
-import freeplaneGTD.GTDChangeListener
+import freeplaneGTD.GTDNodeChangeListener
+import freeplaneGTD.GTDMapChangeListener
 import org.freeplane.features.map.MapController
 import org.freeplane.features.mode.Controller
 
 MapController mapController = Controller.currentController.modeController.mapController
+
+mapController.getMapChangeListeners().findAll {
+    it.getClass().simpleName.equals("GTDMapChangeListener")
+}.each {
+    mapController.removeMapChangeListener(it)
+}
+mapController.addMapChangeListener(new GTDMapChangeListener())
+
 mapController.getNodeChangeListeners().findAll {
-    it.getClass().simpleName.equals("GTDChangeListener")
+    it.getClass().simpleName.equals("GTDNodeChangeListener")
 }.each {
     mapController.removeNodeChangeListener(it)
 }
-mapController.addNodeChangeListener(new GTDChangeListener())
+mapController.addNodeChangeListener(new GTDNodeChangeListener())
