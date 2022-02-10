8b58a91f11ccd26c77aa117ad311a00e207c75f8
GergelyPapp <gergely.papp@itworks.hu>
2022-02-10 11:28:26 +0100

    Added sanity checks, bumped vesrion fixed icons

--- src/freeplaneGTD.addon.mm
+++ src/freeplaneGTD.addon.mm
@@ -1,17 +1,17 @@
-<map version="freeplane 1.8.0">
-  <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-  <node TEXT="Freeplane|GTD+" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1591384204470"
-        LINK="https://www.itworks.hu/freeplanegtd-release/" BACKGROUND_COLOR="#97c7dc">
-    <font SIZE="16" BOLD="true" ITALIC="true"/>
-    <hook NAME="MapStyle">
-      <properties show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false;"/>
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Freeplane|GTD+" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1644488303176" LINK="https://www.itworks.hu/freeplanegtd-release/" BACKGROUND_COLOR="#97c7dc">
+<font SIZE="16" BOLD="true" ITALIC="true"/>
+<hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false;"/>
 
-      <map_styles>
-        <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
-          <font SIZE="24"/>
-          <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
-            <stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="as_parent">
-              <font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1843665549" COLOR="#000000" STYLE="as_parent">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1843665549" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
@@ -22,6 +22,7 @@
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
@@ -57,20 +58,19 @@
 </stylenode>
 </map_styles>
 </hook>
-<attribute_layout NAME_WIDTH="129.74999613314878 pt" VALUE_WIDTH="134.24999599903833 pt"/>
+<attribute_layout NAME_WIDTH="129.75 pt" VALUE_WIDTH="134.25 pt"/>
 <attribute NAME="name" VALUE="freeplaneGTD"/>
-    <attribute NAME="version" VALUE="v3.6.0-alpha2"/>
+<attribute NAME="version" VALUE="v3.6.2"/>
 <attribute NAME="author" VALUE="Gergely Papp"/>
-    <attribute NAME="freeplaneVersionFrom" VALUE="1.8.0"/>
+<attribute NAME="freeplaneVersionFrom" VALUE="1.9.0"/>
 <attribute NAME="freeplaneVersionTo" VALUE=""/>
 <attribute NAME="downloadUrl" VALUE="https://www.itworks.hu/freeplanegtd-release/"/>
 <attribute NAME="changelogUrl" VALUE=""/>
 <attribute NAME="updateUrl" VALUE="https://www.itworks.hu/freeplanegtd-release/version.properties"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -100,16 +100,14 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <edge COLOR="#999999"/>
 <node TEXT="description" FOLDED="true" POSITION="left" ID="ID_1125389651" CREATED="1323057303632" MODIFIED="1541834957912">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -122,13 +120,12 @@
       To translate the description you have to define a translation for the key 'addons.${name}.description'.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node ID="ID_833172562" CREATED="1323057648210" MODIFIED="1453650197342" BACKGROUND_COLOR="#ffff99"><richcontent TYPE="NODE">
 
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -152,13 +149,13 @@
       <font size="2">The completed actions can be marked and filtered, moved to Review folder or Archive folder </font>
     </p>
     <p>
-      
+
     </p>
     <p>
       <font size="2">Markers (icons) can be configured to correspond to contexts </font>
     </p>
     <p>
-      
+
     </p>
     <p>
       <font size="2">Simple action freeplaneGTD.editor is provided.</font>
@@ -171,52 +168,47 @@
 </node>
 <node TEXT="changes" POSITION="left" ID="ID_1824187995" CREATED="1323057303648" MODIFIED="1541834957912">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       Change log of this add-on: append one node for each noteworthy version and put the details for each version into a child node.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="license" FOLDED="true" POSITION="left" ID="ID_1073906233" CREATED="1323057303663" MODIFIED="1541834957912">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       The add-ons's license that the user has to accept before she can install it.
     </p>
     <p>
-      
+
     </p>
     <p>
       The License text has to be entered as a child of the <i>'license'</i>&#160;node, either as plain text or as HTML.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="This add-on is free software: you can redistribute it and/or modify&#xa;it under the terms of the GNU General Public License as published by&#xa;the Free Software Foundation, either version 3 of the License, or&#xa;(at your option) any later version.&#xa;&#xa;This program is distributed in the hope that it will be useful,&#xa;but WITHOUT ANY WARRANTY; without even the implied warranty of&#xa;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&#xa;GNU General Public License for more details." ID="ID_1763974336" CREATED="1323057303679" MODIFIED="1323093766804">
 <edge COLOR="#999999"/>
 </node>
 </node>
 <node TEXT="preferences.xml" FOLDED="true" POSITION="left" ID="ID_456089147" CREATED="1323057303742" MODIFIED="1571863623275">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -226,36 +218,32 @@
       <font color="#000000" face="SansSerif, sans-serif">Every property in the configuration should receive a default value in <i>default.properties</i>&#160;node.</font>
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;&#xa;&lt;preferences_structure&gt;&#xa;         &lt;tabbed_pane&gt;&#xa;                  &lt;tab name=&quot;plugins&quot;&gt;&#xa;                           &lt;separator name=&quot;freeplaneGTD&quot;&gt;&#xa;                &lt;boolean name=&quot;freeplaneGTD_filter_done&quot; min=&quot;0&quot; /&gt;&#xa;                &lt;combo name=&quot;freeplaneGTD_default_view&quot;&gt;&#xa;                    &lt;choice value=&quot;PROJECT&quot; text=&quot;freeplaneGTD_view_project&quot; /&gt;&#xa;                    &lt;choice value=&quot;WHO&quot; text=&quot;freeplaneGTD_view_who&quot; /&gt;&#xa;                    &lt;choice value=&quot;CONTEXT&quot; text=&quot;freeplaneGTD_view_context&quot; /&gt;&#xa;                    &lt;choice value=&quot;WHEN&quot; text=&quot;freeplaneGTD_view_when&quot; /&gt;&#xa;                &lt;/combo&gt;&#xa;                &lt;boolean name=&quot;freeplaneGTD_auto_fold_map&quot; min=&quot;0&quot; /&gt;             &#xa;                           &lt;/separator&gt;&#xa;                  &lt;/tab&gt;&#xa;         &lt;/tabbed_pane&gt;&#xa;&lt;/preferences_structure&gt;" ID="ID_1035150433" CREATED="1413235874574" MODIFIED="1571863623265"/>
 </node>
 <node TEXT="default.properties" POSITION="left" ID="ID_680880217" CREATED="1323057303742" MODIFIED="1541834957912">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       These properties play together with the preferences: Each property defined in the preferences should have a default value in the attributes of this node.
     </p>
   </body>
-</html>
-</richcontent>
-<attribute_layout NAME_WIDTH="151.4999954849483 pt" VALUE_WIDTH="72.74999783188112 pt"/>
+</html></richcontent>
+<attribute_layout NAME_WIDTH="151.5 pt" VALUE_WIDTH="72.75 pt"/>
 <attribute NAME="freeplaneGTD_filter_done" VALUE="false"/>
 <attribute NAME="freeplaneGTD_default_view" VALUE="PROJECT" OBJECT="org.freeplane.features.format.FormattedObject|java.lang.String&amp;#x7c;PROJECT|number:decimal:#0.####"/>
 <attribute NAME="freeplaneGTD_auto_fold_map" VALUE="true"/>
 <attribute NAME="freeplaneGTD_remember_last_position" VALUE="true"/>
 </node>
-<node TEXT="translations" POSITION="left" ID="ID_580707814" CREATED="1496660973904" MODIFIED="1541834957928"><richcontent TYPE="NOTE">
-
+<node TEXT="translations" POSITION="left" ID="ID_580707814" CREATED="1496660973904" MODIFIED="1541834957928"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -273,28 +261,22 @@
       </li>
     </ul>
   </body>
-</html>
-</richcontent>
-<node TEXT="en" ID="ID_509256155" CREATED="1525898306641" MODIFIED="1525898306643">
-<attribute NAME="addons.${name}" VALUE="Freeplane|GTD+"/>
-</node>
+</html></richcontent>
 </node>
 <node TEXT="deinstall" POSITION="left" ID="ID_1357879524" CREATED="1323057303788" MODIFIED="1570906419734">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       List of files and/or directories to remove on uninstall
     </p>
   </body>
-</html>
-</richcontent>
-<attribute_layout NAME_WIDTH="24.749999262392542 pt" VALUE_WIDTH="280.4999916404488 pt"/>
+</html></richcontent>
+<attribute_layout NAME_WIDTH="24.75 pt" VALUE_WIDTH="280.49999 pt"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}.script.xml"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/FreeplaneGTD.groovy"/>
 <attribute NAME="delete" VALUE="${installationbase}/addons/${name}/scripts/FreeplaneShorthand.groovy"/>
@@ -335,18 +317,17 @@
 </node>
 <node TEXT="scripts" POSITION="right" ID="ID_111817827" CREATED="1323057303804" MODIFIED="1541834957938">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       An add-on may contain multiple scripts. The node text defines the script name (e.g. inserInlineImage.groovy). The name must have a suffix of a supported script language like .groovy or .js and may only consist of letters and digits. The script properties have to be configured via attributes:
     </p>
     <p>
-      
+
     </p>
     <p>
       * menuLocation: &lt;locationkey&gt;
@@ -364,7 +345,7 @@
       &#160;&#160;&#160;- This attribute is mandatory
     </p>
     <p>
-      
+
     </p>
     <p>
       * menuTitleKey: &lt;key&gt;
@@ -376,7 +357,7 @@
       &#160;&#160;&#160;- This attribute is mandatory
     </p>
     <p>
-      
+
     </p>
     <p>
       * executionMode: &lt;mode&gt;
@@ -400,7 +381,7 @@
       &#160;&#160;&#160;- This attribute is mandatory
     </p>
     <p>
-      
+
     </p>
     <p>
       * keyboardShortcut: &lt;shortcut&gt;
@@ -418,7 +399,7 @@
       &#160;&#160;&#160;&#160;&#160;In the list only entries with a 'VK_' prefix count. Omit the prefix in the shortcut definition.
     </p>
     <p>
-      
+
     </p>
     <p>
       * Permissions&#160;that the script(s) require, each either false or true:
@@ -454,10 +435,9 @@
       &#160;&#160;- In any case set execute_scripts_without_asking to true unless you want to annoy users.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="ParseShorthand.groovy" ID="ID_900035903" CREATED="1323057833226" MODIFIED="1495225330848">
-<attribute_layout NAME_WIDTH="115.49999655783186 pt" VALUE_WIDTH="89.24999734014281 pt"/>
+<attribute_layout NAME_WIDTH="115.5 pt" VALUE_WIDTH="89.25 pt"/>
 <attribute NAME="menuTitleKey" VALUE="addons.${name}.parseShorthand"/>
 <attribute NAME="menuLocation" VALUE="main_menu_scripting/${name}"/>
 <attribute NAME="executionMode" VALUE="on_single_node"/>
@@ -470,7 +450,7 @@
 <edge COLOR="#999999"/>
 </node>
 <node TEXT="EditTask.groovy" ID="ID_572939250" CREATED="1415043619831" MODIFIED="1497989300199">
-<attribute_layout NAME_WIDTH="120.74999640136969 pt" VALUE_WIDTH="86.99999740719802 pt"/>
+<attribute_layout NAME_WIDTH="120.75 pt" VALUE_WIDTH="87 pt"/>
 <attribute NAME="menuTitleKey" VALUE="addons.${name}.editAction"/>
 <attribute NAME="menuLocation" VALUE="main_menu_scripting/${name}"/>
 <attribute NAME="executionMode" VALUE="on_selected_node"/>
@@ -482,7 +462,7 @@
 <attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
 <node TEXT="ReviewTask.groovy" ID="ID_1165981840" CREATED="1453634472702" MODIFIED="1495225405523">
-<attribute_layout NAME_WIDTH="122.2499963566662 pt" VALUE_WIDTH="83.99999749660499 pt"/>
+<attribute_layout NAME_WIDTH="122.25 pt" VALUE_WIDTH="84 pt"/>
 <attribute NAME="menuTitleKey" VALUE="addons.${name}.reviewTask"/>
 <attribute NAME="menuLocation" VALUE="main_menu_scripting/${name}"/>
 <attribute NAME="executionMode" VALUE="on_single_node"/>
@@ -494,7 +474,7 @@
 <attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
 <node TEXT="ArchiveTask.groovy" ID="ID_1222024407" CREATED="1453634472702" MODIFIED="1495225397428">
-<attribute_layout NAME_WIDTH="122.2499963566662 pt" VALUE_WIDTH="83.99999749660499 pt"/>
+<attribute_layout NAME_WIDTH="122.25 pt" VALUE_WIDTH="84 pt"/>
 <attribute NAME="menuTitleKey" VALUE="addons.${name}.archiveTask"/>
 <attribute NAME="menuLocation" VALUE="main_menu_scripting/${name}"/>
 <attribute NAME="executionMode" VALUE="on_single_node"/>
@@ -506,93 +486,88 @@
 <attribute NAME="execute_scripts_without_network_restriction" VALUE="false"/>
 </node>
 </node>
-<node TEXT="lib" POSITION="right" ID="ID_1003498816" CREATED="1413287263211" MODIFIED="1541834958038"><richcontent TYPE="NOTE">
-
+<node TEXT="lib" POSITION="right" ID="ID_1003498816" CREATED="1413287263211" MODIFIED="1541834958038"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       An add-on may contain any number of nodes containing binary files (normally .jar files) to be added to the add-on's classpath.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The immediate child nodes contain the name of the file, e.g. 'mysql-connector-java-5.1.25.jar'). Put the file into a 'lib' subdirectory of the add-on base directory.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The child nodes of these nodes contain the actual files.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- Any lib file will be extracted in &lt;installationbase&gt;/&lt;addonname&gt;/lib.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The files will be processed in the sequence as seen in the map.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node TEXT="zips" POSITION="right" ID="ID_1927628745" CREATED="1323057303835" MODIFIED="1541834958043">
 <edge COLOR="#999999" WIDTH="3"/>
-<richcontent TYPE="NOTE">
-
+<richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
       An add-on may contain any number of nodes containing zip files.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The immediate child nodes contain a description of the zip. The devtools script releaseAddOn.groovy allows automatic zip creation if the name of this node matches a directory in the current directory.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The child nodes of these nodes contain the actual zip files.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- Any zip file will be extracted in the &lt;installationbase&gt;. Currently, &lt;installationbase&gt; is always Freeplane's &lt;userhome&gt;, e.g. ~/.freeplane/1.3.
     </p>
     <p>
-      
+
     </p>
     <p>
       &#160;- The files will be processed in the sequence as seen in the map.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="icons" ID="ID_1422580718" CREATED="1323094256288" MODIFIED="1497481475102" LINK="resources/zips/icons/"/>
 <node TEXT="lib" ID="ID_1543761795" CREATED="1413298467588" MODIFIED="1497481062534" LINK="../out/lib/"/>
 <node TEXT="templates" ID="ID_1096097283" CREATED="1446325478471" MODIFIED="1497481445935" LINK="resources/zips/templates"/>
 <node TEXT="scripts" ID="ID_1422985646" CREATED="1525898306772" MODIFIED="1525898306773"/>
 </node>
-<node TEXT="images" FOLDED="true" POSITION="right" ID="ID_907142452" CREATED="1323172874273" MODIFIED="1541834958102" LINK="resources/images"><richcontent TYPE="NOTE">
-
+<node TEXT="images" FOLDED="true" POSITION="right" ID="ID_907142452" CREATED="1323172874273" MODIFIED="1541834958102" LINK="resources/images"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
 <html>
   <head>
-    
+
   </head>
   <body>
     <p>
@@ -602,7 +577,7 @@
       The images are saved to the <i>${installationbase}/resources/images</i>&#160;directory.
     </p>
     <p>
-      
+
     </p>
     <p>
       The following images should be present:
@@ -619,8 +594,7 @@
       Images can be added automatically by releaseAddOn.groovy or must be uploaded into the map via the script <i>Tools-&gt;Scripts-&gt;Insert Binary</i>&#160;since they have to be (base64) encoded as simple strings.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node TEXT="${name}.png" ID="ID_1928924406" CREATED="1323059848070" MODIFIED="1323063753585">
 <edge COLOR="#999999"/>
 </node>
--- src/zips/lib/freeplaneGTD/util/IconUtil.groovy
+++ src/zips/lib/freeplaneGTD/util/IconUtil.groovy
@@ -1,22 +1,26 @@
 package freeplaneGTD.util
 
+import groovy.util.logging.Log
+
 import org.freeplane.core.resources.ResourceController
 import org.freeplane.features.icon.UIIcon
 import org.freeplane.features.icon.factory.IconFactory
+import org.freeplane.features.icon.factory.IconStoreFactory
 
 import javax.swing.*
 
+@Log
 class IconUtil {
     static Icon getIcon(String s) {
-        URL iconUrl = ResourceController.getResourceController().getResource("/images/icons/" + s + ".svg")
-        if (!iconUrl) {
-            iconUrl = ResourceController.getResourceController().getResource("/images/IconNotFound.svg")
-        }
-        return IconFactory.getInstance().getIcon(iconUrl)
+        return IconFactory.getInstance().getIcon(getUIIcon(s))
     }
 
     static UIIcon getUIIcon(String s) {
+        try {
+			return IconStoreFactory.ICON_STORE.getUIIcon(s)
+		} catch (Exception error) {
+			log.info("Error looking up icon:"+s)
+		}
         return new UIIcon(s, "icons/" + s + ".svg", "", "?", -1)
     }
-
 }
