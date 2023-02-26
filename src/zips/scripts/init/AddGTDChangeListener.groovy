f7a497f7d183530ecbe13a24f6dd96c289cdbfee
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-02-26 13:57:29 -0300

    inactivate init script if freeplaneGTD is inactive

--- src/zips/scripts/init/AddGTDChangeListener.groovy
+++ src/zips/scripts/init/AddGTDChangeListener.groovy
@@ -1,6 +1,11 @@
 import freeplaneGTD.GtdReportController
 import org.freeplane.features.mode.Controller
 import org.freeplane.features.mode.ModeController
+import org.freeplane.main.addons.AddOnsController
 
-ModeController modeController = Controller.currentModeController
-GtdReportController.install(modeController)
+def isActive = AddOnsController.getController().getInstalledAddOns().find{it.name == 'freeplaneGTD'}?.active
+
+if (isActive){
+    ModeController modeController = Controller.currentModeController
+    GtdReportController.install(modeController)
+}
